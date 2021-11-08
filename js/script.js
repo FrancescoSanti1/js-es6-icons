9e89ef5934c09e7da39758358380644b622ab04e
francesco <francescosanti91@gmail.com>
2021-11-08 15:13:50 +0100

    composizione pagina iniziale

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// creazione pagina di partenza con tutte le icone in pagina
+
+// per ogni elemento dell'array di icone
+listaAnimali.forEach(icona => {
+
+    // creo la struttura html in modo che l'icona venga mostrata in pagina
+    document.getElementById("contenitore-icone").innerHTML += 
+        `<div class="oggetto">
+            <div class="contenuti">
+                <i class="${icona.family} ${icona.prefix}${icona.name} ${icona.color}"></i>
+                <div class="nome-icona">${icona.name}</div>
+            </div>
+        </div>`;
+});
